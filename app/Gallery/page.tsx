742dda9ad0119ddd70d9bb60eb20e776118c7a46
Seema <seemaumesh007@gmail.com>
2025-03-12 16:35:02 +0530

    images added

--- app/Gallery/page.tsx
+++ app/Gallery/page.tsx
@@ -74,14 +74,14 @@ const projects = [
     title: "Workshop",
     category: "Workshop",
     image: "https://github.com/ayushanand2003/Newrro-production-build/blob/main/public/assets/work_gallery/GAT.png?raw=true",
-    description: "Robotic Workshop for students",
+    description: "Robotic Workshop for students at GAT",
   },
   {
     id: 14,
     title: "Workshop",
     category: "Workshop",
     image: "https://github.com/ayushanand2003/Newrro-production-build/blob/main/public/assets/work_gallery/GAT1.png?raw=true",
-    description: "Robotic Workshop for students",
+    description: "Robotic Workshop for students at GAT",
 
   },
   {
@@ -89,21 +89,21 @@ const projects = [
     title: "Workshop",
     category: "Workshop",
     image: "https://github.com/ayushanand2003/Newrro-production-build/blob/main/public/assets/work_gallery/GAT2.png?raw=true",
-    description: "Robotic Workshop for students",
+    description: "Robotic Workshop for students at GAT",
   },
   {
     id: 16,
     title: "University Trainings" ,
     category: "University",
     image: "https://github.com/ayushanand2003/Newrro-production-build/blob/main/public/assets/work_gallery/Global1.png?raw=true",
-    description: "Teaching students about Robotics",
+    description: "Teaching students about Robotics at Global",
   },
   {
     id: 17,
     title: "University Trainings" ,
     category: "University",
     image: "https://github.com/ayushanand2003/Newrro-production-build/blob/main/public/assets/work_gallery/GAT2.png?raw=true",
-    description: "Teaching students about Robotics",
+    description: "Teaching students about Robotics at Global",
 
   },
   {
@@ -111,8 +111,72 @@ const projects = [
     title: "University Trainings" ,
     category: "University",
     image: "https://github.com/ayushanand2003/Newrro-production-build/blob/main/public/assets/work_gallery/Global2.png?raw=true",
-    description: "Teaching students about Robotics",
+    description: "Teaching students about Robotics at Global",
   },
+  {
+    id: 19,
+    title: "University Trainings" ,
+    category: "University",
+    image: "https://github.com/ayushanand2003/Newrro-production-build/blob/main/public/assets/work_gallery/JSS1.png?raw=true",
+    description: "Teaching students about Robotics at JSS",
+  },
+  {
+    id: 20,
+    title: "University Trainings" ,
+    category: "University",
+    image: "https://github.com/ayushanand2003/Newrro-production-build/blob/main/public/assets/work_gallery/JSS2.png?raw=true",
+    description: "Teaching students about Robotics at JSS",
+  },
+  {
+    id: 21,
+    title: "University Trainings" ,
+    category: "University",
+    image: "https://github.com/ayushanand2003/Newrro-production-build/blob/main/public/assets/work_gallery/JSS3.png?raw=true",
+    description: "Teaching students about Robotics at JSS",
+  },
+  {
+    id: 22,
+    title: "University Trainings" ,
+    category: "University",
+    image: "https://github.com/ayushanand2003/Newrro-production-build/blob/main/public/assets/work_gallery/SMVIT1.png?raw=true",
+    description: "Teaching students about Robotics at SMVIT",
+  },
+  {
+    id: 23,
+    title: "University Trainings" ,
+    category: "University",
+    image: "https://github.com/ayushanand2003/Newrro-production-build/blob/main/public/assets/work_gallery/SMVIT2.png?raw=true",
+    description: "Teaching students about Robotics at SMVIT",
+  },
+  {
+    id: 24,
+    title: "University Trainings" ,
+    category: "University",
+    image: "https://github.com/ayushanand2003/Newrro-production-build/blob/main/public/assets/work_gallery/SMVIT3.png?raw=true",
+    description: "Teaching students about Robotics at SMVIT",
+  },
+  {
+    id: 25,
+    title: "University Trainings" ,
+    category: "University",
+    image: "https://github.com/ayushanand2003/Newrro-production-build/blob/main/public/assets/work_gallery/reva2.png?raw=true",
+    description: "Teaching students about Robotics at Reva",
+  },
+  {
+    id: 26,
+    title: "University Trainings" ,
+    category: "University",
+    image: "https://github.com/ayushanand2003/Newrro-production-build/blob/main/public/assets/work_gallery/reva1.JPG?raw=true",
+    description: "Teaching students about Robotics at Reva",
+  },
+  {
+    id: 27,
+    title: "University Trainings" ,
+    category: "University",
+    image: "https://github.com/ayushanand2003/Newrro-production-build/blob/main/public/assets/work_gallery/reva3.png?raw=true",
+    description: "Teaching students about Robotics at Reva",
+  },
+
 
  
 
